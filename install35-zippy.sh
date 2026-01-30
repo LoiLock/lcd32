f8837aabfb5a5e021b497024cd87d143bbbe5ca8
Jochem Hagenaars <jochem.hagenaars@gmail.com>
2026-01-30 15:53:33 +0000

    feat: add add keyboard toggle to install

--- install35-zippy.sh
+++ install35-zippy.sh
@@ -2,7 +2,7 @@
 TARGET_DIR="./backup"
 FILES_TO_COPY=("/boot/firmware/cmdline.txt" "/boot/firmware/config.txt" "/etc/rc.local" "/etc/default/console-setup")
 
-# target dir present?
+# Target dir present
 if [ -d "$TARGET_DIR" ]; then
     echo "Target directory '$TARGET_DIR' exists. Will not touch."
 else
@@ -21,7 +21,7 @@ else
     done
 fi
 
-#now install driver
+# MARK: Drivers
 echo "Installing drivers"
 sudo apt install xserver-xorg-input-evdev xinput-calibrator -y
 sudo cp -rf waveshare35b-v2.dtbo /boot/overlays/waveshare35b-v2.dtbo
@@ -35,6 +35,8 @@ cat /boot/firmware/cmdline.txt cmd_ext.txt > cmdline2.txt
 sudo cp -rf cmdline2.txt /boot/firmware/cmdline.txt
 sudo cp -rf rc.local /etc/rc.local
 sudo cp -rf console-setup /etc/default/console-setup
+
+# MARK: Disable services
 
 echo "Disable unused services"
 sudo systemctl disable exim4 # Mail transfer agent
@@ -50,7 +52,51 @@ sudo touch /etc/cloud/cloud-init.disabled # Disable cloud init - should only be 
 echo "Disable wait for network service"
 sudo systemctl disable NetworkManager-wait-online.service
 
-echo "Now reboot for changes to take effect.."
+# MARK: Install keyboard
+echo "Install keyboard"
+sudo apt install matchbox-keyboard -y
+
+sudo cp -rf toggle-keyboard.sh /usr/bin/toggle-keyboard.sh
+sudo chmod +x /usr/bin/toggle-keyboard.sh
+
+echo "Create .desktop file to open keyboard from taskbar"
+sudo cp -rf toggle-keyboard.desktop /usr/share/raspi-ui-overrides/applications/toggle-keyboard.desktop
+
+echo "copy default taskbar panel config"
+sudo cp -rf /etc/xdg/lxpanel-pi/panels/panel /home/pi/.config/lxpanel-pi/panels/panel
+
+# Panel config path
+PANEL_CONFIG="/home/pi/.config/lxpanel-pi/panels/panel"
+
+# Check if panel config exists
+if [ ! -f "$PANEL_CONFIG" ]; then
+    echo "Error: Config file not found at $PANEL_CONFIG"
+    exit 1
+fi
+
+# Remove any previous versions of this block (between our markers)
+# This prevents duplicates if you run the script multiple times
+sed -i '/# START_KB_PLUGIN/,/# END_KB_PLUGIN/d' "$PANEL_CONFIG"
+
+# Add keyboard toggle button to taskbar
+cat <<EOF >> "$PANEL_CONFIG"
+# START_KB_PLUGIN
+Plugin {
+  type=launchbar
+  Config {
+    Button {
+      id=toggle-keyboard.desktop
+    }
+  }
+}
+# END_KB_PLUGIN
+EOF
+
+echo "Keyboard shortcut added to taskbar"
+
+# MARK: End of script
+
+echo "Reboot for changes to take effect"
 
 
 
